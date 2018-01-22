19dd9f29571c299d1ea720a4a7223fcdd39501f3
Andrew Johnson <ajj@byu.net>
2018-01-22 05:38:46 -0600

    Cleaned up comments

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,21 +3,21 @@ let photoApp = angular.module('PhotoApp', []);
 photoApp.controller('MainController', function($http) {
     let self = this;
 
-    self.listArray = ['mirandas-dairy', 'zoo-gorilla', 'albania-cifteli', 'albania-view', 'mesa-temple', 'yellowstone'];
+    self.listArray = [];
     self.photo = function() {
         $http.get('/photo')
-        .then(function(response) {
-            console.log(response);
-            self.photosList = {};
-            for (let i = 0; i < response.data.length; i++) {
-                self.photosList[response.data[i].name] = response.data[i].likes;
-                console.log('this is photoslst', self.photosList);
-            }
+            .then(function(response) {
+                self.photosList = {};
+                for (let i = 0; i < response.data.length; i++) {
+                    let currentItem = response.data[i];
+                    self.listArray.push(currentItem.name);
+                    self.photosList[currentItem.name] = currentItem.likes;
+                }
 
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
+            })
+            .catch(function(error) {
+                console.log('error getting photos', error);
+            });
     }
     self.photo();
 
@@ -27,14 +27,11 @@ photoApp.controller('MainController', function($http) {
                 name: imageName
             }
         };
-        console.log(imageName);
         $http.get('/photo/likes', config)
             .then(function(response) {
-                console.log('response', response.data[0].likes);
-                self.photosList[response.data[0].name] = response.data[0].likes;
-                console.log('photoslist', self.photosList);
-                console.log('what is here', self.photosList[response.data[0].name]);
-                self.updateLikes(imageName, self.photosList[response.data[0].name] + 1);
+                let currentItem = response.data[0];
+                self.photosList[currentItem.name] = currentItem.likes;
+                self.updateLikes(imageName, self.photosList[currentItem.name] + 1);
             })
             .catch(function(error){
                 console.log('error', error);
@@ -49,12 +46,9 @@ photoApp.controller('MainController', function($http) {
                 currentLikes: currentLikes
             }
         };
-        console.log('in update', imageName, currentLikes);
         $http.put('/photo/likes', config)
             .then(function(response) {
                 self.photosList[imageName] = currentLikes;
-                console.log('response in PUT', response.data);
-                console.log('photoslist', self.photosList);
             })
             .catch(function(error){
                 console.log('error in PUT', error);
