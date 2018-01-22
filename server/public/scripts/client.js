31b71c6ffc881b4163dd4364a0e8142775decd73
Andrew Johnson <ajj@byu.net>
2018-01-22 05:52:41 -0600

    Toggles reason

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,6 +3,14 @@ let photoApp = angular.module('PhotoApp', []);
 photoApp.controller('MainController', function($http) {
     let self = this;
 
+    self.toggleReason = function(photoObject) {
+        if (photoObject.showReason) {
+            photoObject.showReason = false;
+        } else {
+            photoObject.showReason = true;
+        }
+    }
+
     self.listArray = [];
     self.photo = function() {
         $http.get('/photo')
@@ -10,7 +18,11 @@ photoApp.controller('MainController', function($http) {
                 self.photosList = {};
                 for (let i = 0; i < response.data.length; i++) {
                     let currentItem = response.data[i];
-                    self.listArray.push(currentItem.name);
+                    self.listArray.push({
+                        name: currentItem.name,
+                        reason: currentItem.reason,
+                        showReason: false
+                    });
                     self.photosList[currentItem.name] = currentItem.likes;
                 }
 
